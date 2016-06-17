d982c43a4d554302463c22df512239fe02c3d252
Aryan Falahatpisheh <aryan.falahatpisheh@berkeley.edu>
2016-06-17 12:14:19 -0700

    implemented some game code, still need to update world and display

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,8 @@
+import location.Grid;
+import location.GridLocation2D;
+
+import java.util.Scanner;
+
 /**
  * Created by aryan on 6/17/2016.
  */
@@ -7,7 +12,39 @@ public class Game {
     World world;
 
     public void start() {
+        displayer = new Displayer();
+        Grid<GridLocation2D> grid = new Grid<GridLocation2D>();
+        world = new World(grid);
 
+        ZombieGerbil zg1 = new ZombieGerbil(this, world, grid);
+        ZombieGerbil zg2 = new ZombieGerbil(this, world, grid);
+        ZombieGerbil zg3 = new ZombieGerbil(this, world, grid);
+        GridLocation2D loc1 = GridLocation2D.getLocation(5, 5);
+        GridLocation2D loc2 = GridLocation2D.getLocation(3, 5);
+        GridLocation2D loc3 = GridLocation2D.getLocation(3, 7);
+
+        world.addMember(zg1, loc1, true);
+        world.addMember(zg2, loc1, true);
+        world.addMember(zg3, loc1, true);
+
+    }
+
+    public void run() {
+        Scanner reader = new Scanner(System.in);
+        boolean shouldRun = true;
+        while (shouldRun) {
+            System.out.println("How many turns to run? (-1 for exit)");
+            int numTurns = reader.nextInt();
+            if (numTurns == -1) {
+                shouldRun = false;
+            }
+            while (numTurns > 0) {
+                numTurns--;
+                updateGame();
+                updateDisplay();
+            }
+        }
+        System.out.println("Exiting nicely!");
     }
 
     public void stop() {
